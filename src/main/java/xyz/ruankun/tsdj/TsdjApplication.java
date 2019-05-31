0bccf3135cb7d67ec2c87e98e3fd3c0bc3ed30a6
mrruan <qkmc@outlook.com>
2019-05-31 21:33:38 +0800

    create project

---
+++ src/main/java/xyz/ruankun/tsdj/TsdjApplication.java
@@ -0,0 +1,13 @@
+package xyz.ruankun.tsdj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsdjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsdjApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/sql/tsdj.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : 本地
+ Source Server Type    : MySQL
+ Source Server Version : 80011
+ Source Host           : localhost:3306
+ Source Schema         : tsdj
+
+ Target Server Type    : MySQL
+ Target Server Version : 80011
+ File Encoding         : 65001
+
+ Date: 31/05/2019 21:31:37
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
